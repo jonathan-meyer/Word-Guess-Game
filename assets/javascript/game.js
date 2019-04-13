abe83bfee7373a0b4f41f1f96ec38427b7fc2131
JONATHAN MEYER <JONATHAN.MEYER@LIBERTYMUTUAL.COM>
2019-04-12 23:24:00 -0400

    ...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,120 @@
-function handleKeyPress(e) {
-  if (e.key >= "a" && e.key <= "z") {
-    console.log("letter");
-  } else {
-    console.log("other");
-  }
+function isLetter(char) {
+  return char.length == 1 && new RegExp("[a-zA-Z]").test(char);
 }
 
-$(function() {
-  var doc = $(":root");
-  var splash = $("#splash");
-  var game = $("#game");
+function isWhiteSpace(char) {
+  return char.length == 1 && new RegExp("\\s").test(char);
+}
 
-  doc.keyup(function(e) {
-    if (splash.is(":visible")) {
-      splash.hide();
-      game.show();
-    } else {
-      handleKeyPress(e);
+(function($) {
+  $.widget("stej.game", {
+    options: {
+      words: []
+    },
+
+    word: "",
+    maxGuesses: 0,
+    wins: 0,
+    letters: {},
+    guesses: [],
+
+    start: function() {
+      var self = this;
+
+      this.word = this._pickWord();
+      this.maxGuesses = this.word.length * 2;
+      this.letters = {};
+      this.guesses = [];
+
+      console.log("start: " + this.word);
+
+      this.wordEl.empty();
+
+      $.each(this.word.split(""), function() {
+        var letterEl = $("<div>").letter({ value: this });
+        var letterEls = self.letters[this.toLowerCase()];
+
+        self.wordEl.append(letterEl);
+
+        if (Array.isArray(letterEls)) {
+          letterEls.push(letterEl);
+        } else {
+          letterEls = [letterEl];
+        }
+
+        self.letters[this.toLowerCase()] = letterEls;
+      });
+
+      this._update();
+      this.element.show();
+    },
+
+    guess: function(letter) {
+      console.log("guess: ", letter);
+
+      if (this.guesses.length < this.maxGuesses) {
+        if (!this.guesses.includes(letter)) {
+          if (Object.keys(this.letters).includes(letter.toLowerCase())) {
+            $.each(this.letters[letter.toLowerCase()], function() {
+              $(this).letter("reveal");
+            });
+          } else {
+            this.guesses.push(letter);
+          }
+        }
+      }
+
+      if (this.guesses.length >= this.maxGuesses) {
+        alert("The word was: " + this.word);
+        this.start();
+      } else {
+        this._update();
+      }
+    },
+
+    _pickWord: function() {
+      return this.options.words[Math.round((Math.random() * 100) % this.options.words.length)];
+    },
+
+    _create: function() {
+      // console.log("words: ", this.options.words);
+
+      this.wordEl = $("<div>").addClass("word");
+      this.winsEl = $("<span>").addClass("wins");
+      this.guessesEl = $("<span>").addClass("guesses");
+      this.guessesRemainingEl = $("<span>").addClass("guessesRemaining");
+
+      this.element.hide();
+
+      this.element.append(this.wordEl);
+
+      this.element.append(
+        $("<div>")
+          .text("Wins: ")
+          .append(this.winsEl)
+      );
+
+      this.element.append(
+        $("<div>")
+          .text("Guesses: ")
+          .append(this.guessesEl)
+      );
+
+      this.element.append(
+        $("<div>")
+          .text("Guesses Remaining: ")
+          .append(this.guessesRemainingEl)
+      );
+
+      this.element.addClass("game");
+
+      this._update();
+    },
+
+    _update: function() {
+      this.winsEl.text(this.wins);
+      this.guessesEl.text(this.guesses.map(l => l.toUpperCase()).join(" "));
+      this.guessesRemainingEl.text(this.maxGuesses - this.guesses.length);
     }
   });
-});
+})(jQuery);
---
+++ assets/javascript/letter.js
@@ -0,0 +1,42 @@
+(function($) {
+  $.widget("stej.letter", {
+    options: {
+      value: "",
+      revealed: false
+    },
+
+    value: function(value) {
+      if (value === undefined) {
+        return this.options.value;
+      } else {
+        this.options.value = value;
+        this._update();
+      }
+    },
+
+    reveal: function() {
+      // console.log("reveal: ", this.options.value);
+      this.options.revealed = true;
+      this._update();
+    },
+
+    _create: function() {
+      this.element.addClass("letter");
+      this._update();
+    },
+
+    _update: function() {
+      // console.log("letter: ", this.options.value);
+
+      if (this.options.revealed) {
+        this.element.addClass("letter-revealed");
+        this.element.removeClass("letter-hidden");
+        this.element.text(this.options.value);
+      } else {
+        this.element.removeClass("letter-revealed");
+        this.element.addClass("letter-hidden");
+        this.element.text("_");
+      }
+    }
+  });
+})(jQuery);
